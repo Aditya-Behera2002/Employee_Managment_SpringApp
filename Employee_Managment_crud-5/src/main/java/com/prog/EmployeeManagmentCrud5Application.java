cda730f7150cfd316411bb61a99b966c40190cdf
ADITYA LEGION <ADITYA LEGION@DESKTOP-415G7Q8>
2023-12-20 17:02:03 +0530

    crud app

---
+++ Employee_Managment_crud-5/src/main/java/com/prog/EmployeeManagmentCrud5Application.java
@@ -0,0 +1,13 @@
+package com.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagmentCrud5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagmentCrud5Application.class, args);
+	}
+
+}
